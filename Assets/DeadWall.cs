68907ba6af9ef94fe92bc764385124db6a1e05f8
test@.email <ktc@example.com>
2024-10-07 11:59:00 +0900

    ゲームオーバー処理を追加

--- Assets/DeadWall.cs
+++ Assets/DeadWall.cs
@@ -4,21 +4,24 @@ using UnityEngine;
 
 public class DeadWall : MonoBehaviour
 {
+    private GameObject gameManager;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameManager = GameObject.Find("GameManager");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 
     private void OnCollisionEnter(Collision collision)
     {
         // ボールを削除
         Destroy(collision.gameObject);
+        //ゲームオーバー処理
+        gameManager.GetComponent<GameManager>().GameOver();
     }
 }
